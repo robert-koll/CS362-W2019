9cec9d48e7ebd4115b9ca20c932166a9e0e1638e
kollr <kollr@oregonstate.edu>
2019-03-08 18:55:53 -0800

    Added FinalProject files (incorrect)

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -8,8 +8,6 @@ import junit.framework.TestCase;
 
 
 
-
-
 public class UrlValidatorTest extends TestCase {
 
 
@@ -17,32 +15,76 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
+   public boolean TrueAssert(boolean statement, String test_case) {
+	   if (statement) {
+		   return true;
+	   }
+	   else {
+		   System.out.println("TrueAssert failed: " + test_case);
+		   return false;
+	   }
+   }
+   
+   public boolean FalseAssert(boolean statement, String test_case) {
+	   if (statement) {
+		   System.out.println("FalseAssert failed: " + test_case);
+		   return false;
+	   }
+	   else {
+		   return true;
+	   }
+   }
    
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
+	   //You can use this function to implement your manual testing	   
+	   //UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	      //assertTrue(urlVal.isValid("http://www.google.com"));
+	      //assertTrue(urlVal.isValid("http://www.google.com/"));
+       //assertTrue(urlValidator.isValid("http://example.rocks/"));
+       //assertTrue(urlValidator.isValid("http://example.rocks"));
 	   
    }
    
    
-   public void testYourFirstPartition()
+   public void testRandomTest()
    {
-	 //You can use this function to implement your First Partition testing	   
+	 //You can use this function to implement your Random testing	   
 
    }
    
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
-
-   }
-   //You need to create more test cases for your Partitions if you need to 
    
-   public void testIsValid()
+   public void testProgrammingTest()
    {
 	   //You can use this function for programming based testing
 
+       UrlValidator urlValidator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   TrueAssert(urlValidator.isValid("http://www.google.com"), "http");
+       TrueAssert(urlValidator.isValid("https://www.google.com"), "https");
+       
+	   /* for (int i = 0; i < SchemeTests.length; i++) {
+		   if (SchemeTests[i].valid) {
+			   FalseAssert(urlValidator.isValid(SchemeTests[i].item), SchemeTests[i].item);
+		   }
+		   else {
+			   TrueAssert(urlValidator.isValid(SchemeTests[i].item), SchemeTests[i].item);
+		   }
+	   } */
+
    }
+   
+   /* ResultPair[] SchemeTests = {
+		   new ResultPair("http://google.com", true),
+		   new ResultPair("http://www.google.com", true),
+           new ResultPair("ftp://www.google.com", true),
+           new ResultPair("h3t://www.google.com", true),
+           new ResultPair("3ht://www.google.com", false),
+           new ResultPair("http:/www.google.com", false),
+           new ResultPair("http:www.google.com", false),
+           new ResultPair("http/www.google.com", false),
+           new ResultPair("://www.google.com", false)
+   }; */
    
 
 
